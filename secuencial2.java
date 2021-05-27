8aa0de375e8f9e4c5875f91afb5fcf7496de02ae
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-27 09:58:10 -0500

    Create secuencial2.java

---
+++ secuencial2.java
@@ -0,0 +1,21 @@
+/*un equipo de baloncesto necesita un jugador que mida más de 1.94 para ser apto. 
+ * realizar un algoritmo que lea la altura y nos muestre
+ *  un mensaje que indique si cumple con el requisito
+ * 
+ */
+public class condicionalsimple2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double altura;
+        altura = 1.96;
+        
+        if (altura>1.94) {
+        	System.out.print("Apto para jugar");
+        	
+        }
+       
+        
+	}
+
+}
