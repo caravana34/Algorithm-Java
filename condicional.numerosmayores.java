e32613cb01759fe824c0ad0089562a550dfab4f2
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-27 11:29:38 -0500

    Create condicional.numerosmayores.java

---
+++ condicional.numerosmayores.java
@@ -0,0 +1,26 @@
+public class Maximo {
+    public int Maximo() {
+        //tu codigo va aqui
+        int a,b,c;
+        
+        a = 98;
+        b = 76;
+        c = 45;
+        
+        if (a>b){
+            if (a>c){ 
+                return a ;
+                
+            } else {
+                    return c;
+                
+            }
+                    
+                } else if (b>c){
+                     return b ;
+                } else { 
+                return c ;
+            } 
+            
+        }
+     }
