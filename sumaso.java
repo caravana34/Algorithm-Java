9b9eee1071859bd1854d5cb0a81985c57762c698
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-27 09:44:49 -0500

    Create sumaso.java

---
+++ sumaso.java
@@ -0,0 +1,15 @@
+
+public class sumaso {
+
+	public static void main(String[] args) {
+	int a,b,res;
+		
+		a= 456;
+		b= 876;
+		res = a + b;
+		
+		System.out.println("La suma de es  =" + res);
+
+	}
+
+}
