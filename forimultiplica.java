38cfecfcd5f60ea59f8664243337fbbbe5ed37df
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-29 08:30:11 -0500

    Create forimultiplica.java

---
+++ forimultiplica.java
@@ -0,0 +1,17 @@
+//realizar un algoritmo que lea un numero y muestre la tabla de multiplicar 
+public class sucuencialmultiplicar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, r;
+		n = 5;
+		
+		for(int i = 1; i<100; i++) {
+			r = n*i;
+			System.out.println(n + "*" + i + "=" + r);
+			
+		}
+
+	}
+
+}
