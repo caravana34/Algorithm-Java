17ff9588c74f3f855818dd6d54db3727e32a53bc
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-29 10:35:10 -0500

    Create fori2.java

---
+++ fori2.java
@@ -0,0 +1,17 @@
+//realizar un algoritmo que recorra los numeros del 1 al 10 y muestre la suma de ellos
+public class for2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int suma; 
+		suma = 0;
+	
+		for (int i = 1; i<11; i++) {
+
+			suma += i;}
+		System.out.println("suma = " + suma);
+		
+
+	}
+
+}
